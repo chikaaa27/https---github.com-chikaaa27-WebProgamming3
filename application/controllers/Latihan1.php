62cec5f8fab633ba491a029d4b2abc1c4062f8d6
alsabrigita <alsabrigita@gmail.com>
2022-09-30 15:37:22 +0700

    penjumlahan

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,14 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang... selamat belajar Web Programming";
+    }
+    public function penjumlahan($nil1, $nil2)
+    {
+        $this->load->model('Model_latihan1');
+        $hasil = $this->Model_latihan1->jumlah($nil1, $nil2);
+        echo "Hasil Penjumlahan dari". $nil1 ." + " . $nil2 ." = " .$hasil;
+    }
+}
