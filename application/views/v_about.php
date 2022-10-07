9d8235bdc303c5315b0f683cb17ae2fa87a71cc8
alsabrigita <alsabrigita@gmail.com>
2022-10-07 15:46:18 +0700

    menambah about

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Brigita</li>
+        <li>Nama Belakang : Alasavista N</li>
+    </ul> 
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Batu Kidul Rt 002 Rw 004</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Hobi</h4>
+    <ul type="square">
+        <li>Melukis</li>
+        <li>Mendengarkan musik</li>
+    </ul>
+</section>
