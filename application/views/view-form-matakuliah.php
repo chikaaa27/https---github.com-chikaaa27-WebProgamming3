5e95c59277fe20c80fb0539f00ca12b49d79fa74
alsabrigita <alsabrigita@gmail.com>
2022-10-14 14:42:30 +0700

    membuat validasi

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -22,14 +22,16 @@
                 <th>Kode MTK</th>                     
                 <th>:</th>                     
                 <td>                         
-                    <input type="text" name="kode" id="kode">                     
+                    <input type="text" name="kode" id="kode">
+                    <?= form_error('kode', '<small class="text-danger pl-3">', '</small>'); ?>                     
                 </td>                 
             </tr>                 
             <tr>                     
                 <th>Nama MTK</th>                     
                 <td>:</td>                     
                 <td>                         
-                    <input type="text" name="nama" id="nama">                     
+                    <input type="text" name="nama" id="nama"> 
+                    <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>                     
                 </td>                 
             </tr>                 
             <tr>                     
