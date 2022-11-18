a0f81d6b224af786511a53785bb6a34c68289523
alsabrigita <alsabrigita@gmail.com>
2022-11-18 16:29:34 +0700

    membuat kategori buku

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -26,7 +26,13 @@
         <a class="nav-link pb-0" href="<?= base_url('user/anggota'); ?>">
             <i class="fa fa-fw fa book"></i>
             <span>Data Anggota</span></a>
+
+    </li><li class="nav-item">
+        <a class="nav-link pb-0" href="<?= base_url('buku/kategori'); ?>">
+            <i class="fa fa-fw fa book"></i>
+            <span>Kategori</span></a>
     </li>
+
     </li>
     <!-- Divider -->
     <hr class="sidebar-divider mt-3">
